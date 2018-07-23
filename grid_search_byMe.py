aa4318b2b05672d98f94b38cd7e1483a3a26d30f
Filipe do Rego Barros Luz <37091451+FIlipeRBLuz@users.noreply.github.com>
2018-07-22 22:03:29 -0300

    Grid Search Method / K-fold Cross Validation / Initial XGBoost
    Here you can find methods used to tunning the algorithm and forms to validate it.
    Also, I am sharing my first XGboost algorithm, XGboost is an amazing library to tunning your results.

---
+++ grid_search_byMe.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 18:18:43 2018
+
+@author: filipe.luz
+"""
+
+# Grid Search
+
+#KERNEL_SVM
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting kernel svm to the Training set
+from sklearn.svm import SVC
+classifier = SVC( kernel = 'rbf', random_state = 1 )
+classifier.fit(X_train, y_train)
+
+#Predict the Test set Results
+y_pred = classifier.predict(X_test)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix 
+cm = confusion_matrix(y_test, y_pred)
+
+#Advanced method / Applying k-fold cross validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train,
+                             y = y_train, cv = 10 )
+# Mean of model accuracy
+accuracies.mean()
+#standard desviation
+accuracies.std()
+
+# Applying Grid Search, to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [
+        {'C':[1, 10, 100, 1000], 'kernel':['linear']},
+         {'C':[1,10,100,1000],'kernel':['rbf'], 'gamma' : [0.5, 0.1 ,0.01, 0.001, 0.0001]}
+              ]
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10) 
+
+grid_search = grid_search.fit(X_train, y_train)
+#Discoverying the best parameters to the model
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+
+
+#Visualising the Training set Results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1,X2 = np.meshgrid(np.arange(start = X_set[:, 0 ].min()-1,
+                              stop = X_set[:, 0 ].max()+1,
+                              step = 0.01),
+                    np.arange(start = X_set[:,1].min(),
+                              stop = X_set[:, 1].max(),
+                              step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),
+                                                  X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+
+plt.title('Kernel SVM (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+  
